0f29f93bc5ea5623f0b5cc609475154c1b206258
xcoolz <xcoolz@gmail.com>
2017-04-10 17:26:25 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <?php
-echo "Hello LINE BOT";
+
+$access_token = '1a3bQ341aEF6/M21JlXlE0aIpzvjnTyL0/aylNWA6lA+jUeRmw5asVZPeKnsC2VEdzQDmqeqPAS6Q9DVwBClS1S4Vc2hW8XhEhDdyXE+nfl3WsHC1cox4oSJl4vsx2Dwa3ysBnDySEyaKNR2x8b1KQdB04t89/1O/w1cDnyilFU=';
+
+$url = 'https://api.line.me/v1/oauth/verify';
+
+$headers = array('Authorization: Bearer ' . $access_token);
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);
+curl_close($ch);
+
+echo $result;
+
 ?>
