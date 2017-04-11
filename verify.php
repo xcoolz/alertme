f92841cbf910df4766a2a63d8102f11451aa7500
xcoolz <xcoolz@gmail.com>
2017-04-11 15:32:31 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,10 +1,8 @@
 <?php
-echo "proxy";
-$proxy = 'http://fixie:QoWxhK5CDYdiYPl@velodrome.usefixie.com:80';
-$proxyauth = 'fixie:QoWxhK5CDYdiYPl';
+$proxy = 'http://username:password@proxyurl:port';
+$proxyauth = 'username:password';
 
-$access_token = '1a3bQ341aEF6/M21JlXlE0aIpzvjnTyL0/aylNWA6lA+jUeRmw5asVZPeKnsC2VEdzQDmqeqPAS6Q9DVwBClS1S4Vc2hW8XhEhDdyXE+nfl3WsHC1cox4oSJl4vsx2Dwa3ysBnDySEyaKNR2x8b1KQdB04t89/1O/w1cDnyilFU=';
-
+$access_token = 'your_access_token';
 $url = 'https://api.line.me/v1/oauth/verify';
 
 $headers = array('Authorization: Bearer ' . $access_token);
